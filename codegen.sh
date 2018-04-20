14a0e46e896beebaa9d7dcda01750bf2a2e2e7b3
onyiny-ang <ltulloch@redhat.com>
2018-04-20 17:26:12 -0400

    adds boiler plate to all necessary files

--- codegen.sh
+++ codegen.sh
@@ -1,3 +1,19 @@
+# Copyright 2018 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
 #!/bin/bash
 
 vendor/k8s.io/code-generator/generate-groups.sh deepcopy github.com/pmorie/cluster-registry-crd/pkg/client github.com/pmorie/cluster-registry-crd/pkg/apis clusterregistry:v1alpha1
